b766989cfdcb004173508cfe985b32abca94fdae
gregtorrington <gregt@aleios.com>
2025-09-08 13:16:03 +0200

    fix: including currencies in general report

--- src/pages/dashboard/dash-home.tsx
+++ src/pages/dashboard/dash-home.tsx
@@ -9,6 +9,7 @@ import { formatAmount } from "@/data/currencies";
 
 export default function HomeDashboardPage() {
     const {club} = useContext(ClubContext) as ClubContextType
+    console.log('club', club)
     const {data, isLoading} = useGeneralReportingQuery(club?.club_account_id as string)
     // const {data: r}= useRegistrationBillingReportingQuery(club?.club_account_id as string)
     const navigate = useNavigate()
@@ -71,7 +72,7 @@ export default function HomeDashboardPage() {
                         </div>
                     </CardHeader>
                     <CardContent>
-                        <div className="text-center bg-muted rounded-full p-4 mx-auto w-12 h-12 justify-center items-center flex">{formatAmount(data?.report.total_registration_fees_due_by_pending_members)}</div>
+                        <div className="text-center bg-muted rounded-full p-4 mx-auto w-12 h-12 justify-center items-center flex">{formatAmount(data?.report.total_registration_fees_due_by_pending_members, club?.currency)}</div>
                     </CardContent>
                 </Card>
 
@@ -101,7 +102,7 @@ export default function HomeDashboardPage() {
                         </div>
                     </CardHeader>
                     <CardContent>
-                        <div className="text-center bg-muted rounded-full p-4 mx-auto w-12 h-12 justify-center items-center flex">{formatAmount(data?.report.total_extra_fees_owed_by_registered_members)}</div>
+                        <div className="text-center bg-muted rounded-full p-4 mx-auto w-12 h-12 justify-center items-center flex">{formatAmount(data?.report.total_extra_fees_owed_by_registered_members, club?.currency)}</div>
                     </CardContent>
                 </Card>
             </div>
