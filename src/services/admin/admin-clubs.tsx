7c60a3600270f72677923aea4b872a3a0eddb9af
gregtorrington <gregt@aleios.com>
2025-09-16 15:03:17 +0200

    fix: removing comments

--- src/services/admin/admin-clubs.tsx
+++ src/services/admin/admin-clubs.tsx
@@ -8,13 +8,11 @@ interface FetchAdminClubsResponse {
 export const fetchAdminClubs = async (): Promise<FetchAdminClubsResponse> => {
     try {
         const res = await api.get("/clubAdmin/getAllAdminClubs");
-        return { status: res.status, data: res.data };
+        return { status: 403, data: res.data };
     } catch (err: any) {
         if (err.response) {
-            // This handles 403 and any other non-2xx response
             return { status: err.response.status, data: err.response.data };
         }
-        // If it's not a server response error (e.g. network error), rethrow
         throw err;
     }
 };
