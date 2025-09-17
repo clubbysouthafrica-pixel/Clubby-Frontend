253ad2235c890e6920d83c290bb776ee5b4da7d8
gregtorrington <gregt@aleios.com>
2025-09-17 18:05:47 +0200

    fix: improving billing

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -1,19 +1,12 @@
 import * as React from "react"
 import {
-  AudioWaveform,
-  CircleAlertIcon,
-  Command,
-  GalleryVerticalEnd,
-  HeadsetIcon,
   HomeIcon,
-  HouseIcon,
   UserPlusIcon,
   UsersIcon,
 } from "lucide-react"
 import { NavMain } from "@/components/nav-main"
-// import { NavProjects } from "@/components/nav-projects"
 import { NavUser } from "@/components/nav-user"
-import {ClubSwitcher} from "@/components/club-switcher.tsx"
+import { ClubSwitcher } from "@/components/club-switcher.tsx"
 import {
   Sidebar,
   SidebarContent,
@@ -23,115 +16,87 @@ import {
 } from "@/components/ui/sidebar"
 import { useFetchAdminClubs } from "@/queries/admin/clubs"
 import { ClubContext, ClubContextType } from "@/context/ClubContext"
-// import { useNavigate } from "react-router-dom";
-
-// This is sample data.
-const data = {
-  user: {
-    name: "shadcn",
-    email: "m@example.com",
-    avatar: "/avatars/shadcn.jpg",
-  },
-  clubs: [
-    {
-      name: "Rowing Club Seapoint",
-      logo: GalleryVerticalEnd,
-      plan: "Enterprise",
-    },
-    {
-      name: "Tennis Milnerton",
-      logo: AudioWaveform,
-      plan: "Startup",
-    },
-    {
-      name: "Rugby Club",
-      logo: Command,
-      plan: "Free",
-    },
-  ],
-  navMain: [
-    {
-      title: "Club",
-      url: "/manage/club",
-      icon: HomeIcon,
-      isActive: true,
-      items: [
-        {
-          title: "Home",
-          url: "/"
-        },
-        {
-          title: "Manage Club",
-          url: "/manage/club",
-        },
-      ],
-    },
-    {
-      title: "Members",
-      url: "/manage/members",
-      icon: UsersIcon,
-      items: [
-        {
-          title: "Members",
-          url: "/manage/members",
-        },
-        {
-          title: "Registration Reports",
-          url: "/manage/members/report",
-        },
-      ],
-    },
-    {
-      title: "Registrations",
-      url: "/manage/registrations",
-      icon: UserPlusIcon,
-      items: [
-        {
-          title: "Create Form",
-          url: "/manage/registrations/forms",
-        },
-      ],
-    },
-  ],
-  personal: [
-    {
-      name: "My Clubs",
-      url: "/myclubs",
-      icon: HouseIcon,
-    },
-    {
-      name: "About",
-      url: "/about",
-      icon: CircleAlertIcon,
-    },
-    {
-      name: "Contact Us",
-      url: "/contactus",
-      icon: HeadsetIcon,
-    }
-  ],
-}
+import { AuthContext, AuthContextType } from "@/context/AuthContext";
+import { useGetProfileQuery } from "@/queries/profile"
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
-  const {club} = React.useContext(ClubContext) as ClubContextType
+  const { club } = React.useContext(ClubContext) as ClubContextType
   const { data: clubData, isLoading: loadingClubs } = useFetchAdminClubs()
+
+  const { isAdmin } = React.useContext(AuthContext) as AuthContextType
+  const { data: profile } = useGetProfileQuery(isAdmin)
+
+  const [userData, setUserData] = React.useState({
+    name: "",
+    email: "",
+    avatar: "/avatars/shadcn.jpg",
+  })
+
+  React.useEffect(() => {
+    if (profile) {
+      setUserData({
+        name: `${profile?.first_name ?? ""} ${profile?.surfname ?? ""}`,
+        email: profile.email ?? "",
+        avatar: "/avatars/shadcn.jpg",
+      })
+    }
+  }, [profile, club])
 
   return (
     <Sidebar collapsible="icon" {...props}>
       <SidebarHeader>
-        { !loadingClubs &&
+        {!loadingClubs &&
           <ClubSwitcher clubs={((clubData as any).data as any)?.items} />
         }
       </SidebarHeader>
       <SidebarContent>
-        {
-          club?.club_account_id &&
-          <NavMain items={data.navMain} />
-        }
-        {/* <NavProjects projects={data.personal} /> */}
+        {club?.club_account_id && <NavMain items={[
+          {
+            title: "Club",
+            url: "/manage/club",
+            icon: HomeIcon,
+            isActive: true,
+            items: [
+              {
+                title: "Home",
+                url: "/"
+              },
+              {
+                title: "Manage Club",
+                url: "/manage/club",
+              },
+            ],
+          },
+          {
+            title: "Members",
+            url: "/manage/members",
+            icon: UsersIcon,
+            items: [
+              {
+                title: "Members",
+                url: "/manage/members",
+              },
+              {
+                title: "Registration Reports",
+                url: "/manage/members/report",
+              },
+            ],
+          },
+          {
+            title: "Registrations",
+            url: "/manage/registrations",
+            icon: UserPlusIcon,
+            items: [
+              {
+                title: "Create Form",
+                url: "/manage/registrations/forms",
+              },
+            ],
+          },
+        ]} />}
       </SidebarContent>
       <SidebarFooter>
-        <NavUser user={data.user} />
+        <NavUser user={userData} />
       </SidebarFooter>
       <SidebarRail />
     </Sidebar>
