71c52ffe3bc43855f4b09014ca235ffb763cb7ac
gregtorrington <gregt@aleios.com>
2025-09-17 19:10:29 +0200

    chore: logging removed

--- src/pages/profile/settings.tsx
+++ src/pages/profile/settings.tsx
@@ -47,7 +47,6 @@ export default function SettingsPage() {
     const {isAdmin} = useContext(AuthContext) as AuthContextType
     const {mutate, isPending} = useUpdateProfileMutation(isAdmin)
     const {data} = useGetProfileQuery(isAdmin)
-    console.log(data)
     // const {theme, setTheme} = useContext(ThemeProviderContext)
     const [accountSettings, setAccountSettings] = useState({
         first_name: "",
