a09f45465ff7b831a3a9d0c2d47d951dccfa275c
gregtorrington <gregt@aleios.com>
2025-09-22 13:49:17 +0200

    fix: frontend

--- src/components/admin/members/members/selected-members.tsx
+++ src/components/admin/members/members/selected-members.tsx
@@ -46,10 +46,6 @@ export default function SelectedMember({
 
     console.log(transactions)
 
-    if (isLoading) {
-        return (<div>kke</div>)
-    }
-
     return (
         <Dialog
             open={open}
@@ -61,6 +57,8 @@ export default function SelectedMember({
                 }
             }}
         >
+
+
             <DialogContent className="max-w-4xl w-full p-6">
                 <DialogHeader className="flex justify-between">
                     <div className="flex items-end space-x-2">
@@ -70,12 +68,14 @@ export default function SelectedMember({
                         </DialogTitle>
                     </div>
                 </DialogHeader>
+
                 <Label className="my-2 m-0">Outstanding amount: {formatAmount(selectedMember.outstanding_amount, currency)}</Label>
 
                 <Tabs value={selectedTab} onValueChange={setSelectedTab} className="mt-0">
                     <TabsList>
                         <TabsTrigger className="w-[150px]" value="registered-members">Club information</TabsTrigger>
                         <TabsTrigger className="w-[150px]" value="pending-members">Club fees</TabsTrigger>
+                        <TabsTrigger className="w-[150px]" value="transactions">Transactions</TabsTrigger>
                     </TabsList>
 
                     {selectedTab === "registered-members" && (
@@ -88,53 +88,99 @@ export default function SelectedMember({
                             This section shows the club fees owed and paid by this member at the time of registration.
                         </DialogDescription>
                     )}
+                    {selectedTab === "transactions" && (
+                        <DialogDescription className="mt-2 mb-4">
+                            This section shows transactions associated with this member and the club.
+                        </DialogDescription>
+                    )}
 
                     <div className="overflow-hidden rounded-lg border">
-                        <Table>
-                            <TableHeader className="bg-muted sticky top-0 z-10">
-                                <TableRow>
-                                    <TableHead className="align-middle px-2 py-2 w-1/2">
-                                        Field
-                                    </TableHead>
-                                    <TableHead className="align-middle px-2 py-2 w-1/2">
-                                        Value
-                                    </TableHead>
-                                </TableRow>
-                            </TableHeader>
-                            <TableBody>
-                                {selectedTab === "registered-members" &&
-                                    selectedMember.meta_standard.map((key: any) => (
-                                        <TableRow key={key.field_name}>
-                                            <TableCell className="align-middle px-2 py-2">
-                                                {key.field_name}
-                                            </TableCell>
-                                            <TableCell className="align-middle px-2 py-2">
-                                                {
-                                                    key.value === "true" && key.type === "STANDARD_CHECKBOX" ? `✅`
-                                                        : key.value === "true" && key.type === "STANDARD_CHECKBOX" ? `❌` : key.value
-                                                }
-                                            </TableCell>
-                                        </TableRow>
-                                    ))}
-                                {selectedTab === "pending-members" &&
-                                    selectedMember.meta_billing.map((key: any) => (
-                                        <TableRow key={key.field_name}>
-                                            <TableCell className="align-middle px-2 py-2">
-                                                {key.field_name}
-                                            </TableCell>
-                                            {key.type === "BILLING_DROPDOWN" ? (
-                                                <TableCell className="align-middle px-2 py-2">
-                                                    {key.label_value} ({formatAmount(key.value, currency)})
+                        {selectedTab === "registered-members" || selectedTab === "pending-members" &&
+                            <Table>
+                                <TableHeader className="bg-muted sticky top-0 z-10">
+                                    <TableRow>
+                                        <TableHead className="text-center px-2 py-2 w-1/2">
+                                            Field
+                                        </TableHead>
+                                        <TableHead className="text-center px-2 py-2 w-1/2">
+                                            Value
+                                        </TableHead>
+                                    </TableRow>
+                                </TableHeader>
+                                <TableBody>
+                                    {selectedTab === "registered-members" &&
+                                        selectedMember.meta_standard.map((key: any) => (
+                                            <TableRow key={key.field_name}>
+                                                <TableCell className="text-center px-2 py-2">
+                                                    {key.field_name}
                                                 </TableCell>
-                                            ) : (
-                                                <TableCell className="align-middle px-2 py-2">
-                                                    {formatAmount(key.value, currency)}
+                                                <TableCell className="text-center px-2 py-2">
+                                                    {
+                                                        key.value === "true" && key.type === "STANDARD_CHECKBOX" ? `✅`
+                                                            : key.value === "true" && key.type === "STANDARD_CHECKBOX" ? `❌` : key.value
+                                                    }
                                                 </TableCell>
-                                            )}
-                                        </TableRow>
-                                    ))}
-                            </TableBody>
-                        </Table>
+                                            </TableRow>
+                                        ))}
+                                    {selectedTab === "pending-members" &&
+                                        selectedMember.meta_billing.map((key: any) => (
+                                            <TableRow key={key.field_name}>
+                                                <TableCell className="text-center px-2 py-2">
+                                                    {key.field_name}
+                                                </TableCell>
+                                                {key.type === "BILLING_DROPDOWN" ? (
+                                                    <TableCell className="text-center px-2 py-2">
+                                                        {key.label_value} ({formatAmount(key.value, currency)})
+                                                    </TableCell>
+                                                ) : (
+                                                    <TableCell className="text-center px-2 py-2">
+                                                        {formatAmount(key.value, currency)}
+                                                    </TableCell>
+                                                )}
+                                            </TableRow>
+                                        ))}
+                                </TableBody>
+                            </Table>
+                        }
+                        {selectedTab === "transactions" &&
+                            <Table>
+                                <TableHeader className="bg-muted sticky top-0 z-10">
+                                    <TableRow>
+                                        <TableHead className="text-center w-1/4">
+                                            Date
+                                        </TableHead>
+                                        <TableHead className="text-center w-1/4">
+                                            Payment type
+                                        </TableHead>
+                                        <TableHead className="text-center w-1/4">
+                                            Amount
+                                        </TableHead>
+                                        <TableHead className="text-center w-1/4">
+                                            Status
+                                        </TableHead>
+                                    </TableRow>
+                                </TableHeader>
+                                <TableBody>
+                                    {
+                                        transactions.transactions.map((key: any) => (
+                                            <TableRow key={key.transaction_id}>
+                                                <TableCell className="text-center w-1/4">
+                                                    {key.date}
+                                                </TableCell>
+                                                <TableCell className="text-center w-1/4">
+                                                    {key.payment_type}
+                                                </TableCell>
+                                                <TableCell className="text-center w-1/4">
+                                                    {formatAmount(key.amount, currency)}
+                                                </TableCell>
+                                                <TableCell className={`text-center font-bold w-1/4 ${key.status === "PENDING" ? "text-red-500" : "text-green-500"}`}>
+                                                    {key.status}
+                                                </TableCell>
+                                            </TableRow>
+                                        ))}
+                                </TableBody>
+                            </Table>
+                        }
                     </div>
 
                 </Tabs>
