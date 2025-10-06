60cc2f0b5bcf729256277083662be60a422f978f
Ryan <ryan@alphaburg.co.za>
2025-10-06 20:40:57 +0200

    fixed

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -43,6 +43,7 @@ export function LoginForm({
       const isOnboarded = await login(isAdminLogin, email, password)
       if (isAdminLogin) {
         localStorage.setItem("isAdmin","true")
+        navigate("/")
         return
       }
 
