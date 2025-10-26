5681076d2e690b65178485db4526d949623b70d1
gregtorrington <gregt@aleios.com>
2025-10-26 15:00:20 +0100

    fix: frontend

--- src/components/admin/members/members/current_member_registration.tsx
+++ src/components/admin/members/members/current_member_registration.tsx
@@ -76,9 +76,9 @@ export function CurrentMemberRegistration({
               if (field.type === "BILLING") {
                 return (
                   <div key={field.label} className="flex flex-col gap-2">
-                    <Label className="text-[12px] font-semibold">{field.label} {field.quantity ? `(x${field.quantity})` : null}:</Label>
+                    <Label className="text-[12px] font-semibold">{field.label}:</Label>
                     <Label className="text-[12px] border-b-2 border-gray-300 pb-1 w-200">
-                      {field.value}
+                      {field.value} {field.quantity ? `(x${field.quantity})` : null}
                     </Label>
                   </div>
                 );
