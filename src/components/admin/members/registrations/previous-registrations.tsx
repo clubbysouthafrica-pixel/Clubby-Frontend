267f23c2995705f1fbc94edb72a1b41941988192
gregtorrington <gregt@aleios.com>
2026-02-16 08:14:56 +0200

    feat: improving frontend

--- src/components/admin/members/registrations/previous-registrations.tsx
+++ src/components/admin/members/registrations/previous-registrations.tsx
@@ -328,7 +328,7 @@ export default function PreviousMembersList({
                     </TableCell>
                     <TableCell className="text-center w-[150px]">
                       {member?.last_season_registration || !member?.deregistered_on
-                        ? <Badge variant="secondary" className="text-xs">Previous Season Registration</Badge>
+                        ? <Badge className="bg-red-100 text-red-800 border-red-300">Previous Season Registration</Badge>
                         : new Date(member?.deregistered_on).toLocaleString()
                       }
                     </TableCell>
