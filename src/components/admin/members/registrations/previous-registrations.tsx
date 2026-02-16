017b8099174c2db05fb0f90096a67219d43b6ba5
gregtorrington <gregt@aleios.com>
2026-02-16 08:09:47 +0200

    feat: improving frontend

--- src/components/admin/members/registrations/previous-registrations.tsx
+++ src/components/admin/members/registrations/previous-registrations.tsx
@@ -327,9 +327,10 @@ export default function PreviousMembersList({
                       )}
                     </TableCell>
                     <TableCell className="text-center w-[150px]">
-                      {member.deregistered_on
-                        ? new Date(member.deregistered_on).toLocaleString()
-                        : <Badge variant="secondary" className="text-xs">Previous Season Registration</Badge>}
+                      {member?.last_season_registration || !member?.deregistered_on
+                        ? <Badge variant="secondary" className="text-xs">Previous Season Registration</Badge>
+                        : new Date(member?.deregistered_on).toLocaleString()
+                      }
                     </TableCell>
                     {clubMembers?.filters
                       ?.filter((col: any) => activeColumnKeys.includes(col.key))
