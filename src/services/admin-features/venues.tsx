3e1a2b178295b2293b13486d14799a445e1a9d47
gregtorrington <gregt@aleios.com>
2026-02-26 15:02:07 +0200

    feat: improving frontend

---
+++ src/services/admin-features/venues.tsx
@@ -0,0 +1,23 @@
+import { api } from "@/services/admin-features/api.tsx";
+
+export interface CreateVenueRequest {
+    venue_name: string;
+    smallest_booking_unit: number;
+    max_daily_booking_time: number | null;
+    times: Array<{
+        day_of_week: number;
+        start_time: string;
+        end_time: string;
+        is_closed?: boolean;
+    }>;
+}
+
+export const createVenue = (createVenueRequest: CreateVenueRequest): Promise<any> => {
+    return api.post("/venues/createVenue", createVenueRequest)
+        .then(res => res.data);
+}
+
+export const getVenues = (clubAccountId: string): Promise<any> => {
+    return api.get(`/venues/getVenues?club_account_id=${clubAccountId}`)
+        .then(res => res.data);
+}
