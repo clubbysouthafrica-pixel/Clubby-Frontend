3882deab4ee003d5df7c1bef1e6cdb840ef9c1d9
gregtorrington <gregt@aleios.com>
2026-04-12 19:29:21 +0200

    feat: improving backend

--- src/services/events.ts
+++ src/services/events.ts
@@ -35,9 +35,7 @@ export interface RegisterEventResponse {
 }
 
 export const getEvents = (clubAccountId: string): Promise<GetEventsResponse> => {
-  return api
-    .get(`/events/getEvents?club_account_id=${clubAccountId}&include_all=true`)
-    .then((res) => res.data);
+  return api.get(`/events/getEvents?club_account_id=${clubAccountId}`).then((res) => res.data);
 };
 
 export const getEventRegistrations = (
